02ae9907b8f9afcf4dd715346b37a47c9e3476d1
garciamorenc <garciamorenc@gmail.com>
2026-06-21 09:28:07 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'plan-mc-v1';
+const CACHE = 'plan-mc-v2';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
